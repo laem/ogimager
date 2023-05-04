c23ef539bc1f1f1cfcc9ccd1a884753bb0e22bcc
Mael <mael.thomas@data.gouv.fr>
2023-05-04 16:57:32 +0200

    Message de lancement du serveur plus sympa

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ app.get('/', function (req, res) {
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log('App listening at http://%s:%s', host, port)
+  console.log(
+    'Ready to capture screenshots, listening at http://%s:%s',
+    host,
+    port
+  )
 })
 
 let browser
